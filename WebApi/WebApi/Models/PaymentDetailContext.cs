7c6fdf3882a6fc383c59300481745eea8dc1922f
Hugo <hugo.rock20@hotmail.com>
2019-01-20 22:39:57 -0500

    add new package microsoft.entityframeworkcore and class in model/paymentdetail

--- WebApi/WebApi/Models/PaymentDetailContext.cs
+++ WebApi/WebApi/Models/PaymentDetailContext.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace WebApi.Models
 {
-    public class PaymentDetailContext
+    public class PaymentDetailContext : DbContext
     {
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options): base(options)
+        {
+
+        }
     }
 }
