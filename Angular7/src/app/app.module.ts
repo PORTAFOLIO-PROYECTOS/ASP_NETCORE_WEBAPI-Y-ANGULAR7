18167b99624d4ac161cfd5d1521ad2847b7267f0
Hugo <hugo.rock20@hotmail.com>
2019-01-22 23:09:09 -0500

    delete files and new add new files

--- Angular7/src/app/app.module.ts
+++ Angular7/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PaymentDetailsComponent } from './payment-details/payment-details.component';
+import { PaymentDetailsListComponent } from './payment-details/payment-details-list/payment-details-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaymentDetailsComponent,
+    PaymentDetailsListComponent
   ],
   imports: [
     BrowserModule
