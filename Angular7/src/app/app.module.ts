54a58435abdbbe377beedfbd42c646231d9264c8
Hugo <hugo.rock20@hotmail.com>
2019-01-24 23:44:32 -0500

    view and more changes in flies

--- Angular7/src/app/app.module.ts
+++ Angular7/src/app/app.module.ts
@@ -5,15 +5,13 @@ import { AppComponent } from './app.component';
 import { PaymentDetailsComponent } from './payment-details/payment-details.component';
 import { PaymentDetailComponent } from './payment-details/payment-detail/payment-detail.component';
 import { PaymentDetailListComponent } from './payment-details/payment-detail-list/payment-detail-list.component';
-import { SComponent } from './s/s.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PaymentDetailsComponent,
     PaymentDetailComponent,
-    PaymentDetailListComponent,
-    SComponent
+    PaymentDetailListComponent
   ],
   imports: [
     BrowserModule
