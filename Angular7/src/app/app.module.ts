12c5dd6b34535f5addc4953533015e0e2351b8fd
Hugo <hugo.rock20@hotmail.com>
2019-01-24 23:36:44 -0500

    ng g s shared/payment-detail --spec=false

--- Angular7/src/app/app.module.ts
+++ Angular7/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { PaymentDetailsComponent } from './payment-details/payment-details.component';
 import { PaymentDetailComponent } from './payment-details/payment-detail/payment-detail.component';
 import { PaymentDetailListComponent } from './payment-details/payment-detail-list/payment-detail-list.component';
+import { SComponent } from './s/s.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PaymentDetailsComponent,
     PaymentDetailComponent,
-    PaymentDetailListComponent
+    PaymentDetailListComponent,
+    SComponent
   ],
   imports: [
     BrowserModule
