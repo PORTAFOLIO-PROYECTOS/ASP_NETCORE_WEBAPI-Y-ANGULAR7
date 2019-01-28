4c330ce31aca85833adc0c7b1ce08142d93170e0
Hugo <hugo.rock20@hotmail.com>
2019-01-27 23:08:01 -0500

    desing form

--- Angular7/src/app/app.module.ts
+++ Angular7/src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { PaymentDetailsComponent } from './payment-details/payment-details.component';
-import { PaymentDetailComponent } from './payment-details/payment-detail/payment-detail.component';
-import { PaymentDetailListComponent } from './payment-details/payment-detail-list/payment-detail-list.component';
-import { PaymentDetailService } from './shared/payment-detail.service';
+import { AppComponent } from "./app.component";
+import { PaymentDetailsComponent } from "./payment-details/payment-details.component";
+import { PaymentDetailComponent } from "./payment-details/payment-detail/payment-detail.component";
+import { PaymentDetailListComponent } from "./payment-details/payment-detail-list/payment-detail-list.component";
+import { PaymentDetailService } from "./shared/payment-detail.service";
 
 @NgModule({
   declarations: [
@@ -14,10 +15,8 @@ import { PaymentDetailService } from './shared/payment-detail.service';
     PaymentDetailComponent,
     PaymentDetailListComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule],
   providers: [PaymentDetailService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
